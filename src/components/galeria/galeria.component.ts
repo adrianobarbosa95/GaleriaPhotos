52c9024933303de61c5822f38dac6aee6e27d908
Adriano Barbosa <adrianobarbosa95@gmail.com>
2023-02-19 23:44:43 -0300

    Adiciona botoes e finaliza funcionalidades

--- src/components/galeria/galeria.component.ts
+++ src/components/galeria/galeria.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-galeria',
   templateUrl: './galeria.component.html',
-  styleUrls: ['./galeria.component.css']
+  styleUrls: ['./galeria.component.css'],
 })
 export class GaleriaComponent {
+  @Input() photos: string[] = [];
+  position: number = 0;
+  @Input() legend: string[] = [];
+  first(): void {
+    this.position = 0;
+  }
+  previous( ): void {
 
+
+    this.position--;
+     
+  }
+  next(): void {
+    this.position++;
+  }
+  last(): void {
+    this.position = this.photos.length - 1;
+  }
+
+  desativar(){
+
+
+  }
 }
